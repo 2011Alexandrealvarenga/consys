12b8aab80d6e14e84f2029e3c99647fe27747b1a
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-12-27 13:51:34 -0300

    carro - pagina estrutura

--- carro.php
+++ carro.php
@@ -20,17 +20,64 @@
             </div>
         </div>
     </header>
-    <section class="lista-carro">
+    <section class="pagina-carro">
         <div class="container">
             <div class="row">                
                 <div class="col-12 my-4">
                     pagina carro
                 </div> 
             </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="content-img">
+                        <img src="assets/img/fit.jpg" class="destaque" alt="">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="content-desc">
+                        <div class="dados">
+                            <div class="item">
+                                <span class="carac">Modelo</span>
+                                <span class="carac-item">Fit</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Ano Fábricação</span>
+                                <span class="carac-item">2007</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Ano Modelo</span>
+                                <span class="carac-item">2008</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Cor</span>
+                                <span class="carac-item">Verde</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Câmbio</span>
+                                <span class="carac-item">Automático</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Placa final</span>
+                                <span class="carac-item">5</span>
+                            </div>
+                            <div class="item">
+                                <span class="carac">Quilometragem</span>
+                                <span class="carac-item">125.000</span>
+                            </div>
+                        </div>
+                        <div class="item-valor">
+                            <span class="carac">Valor</span><br>
+                            <span class="carac-item">R$ 25.000,00</span><br>
+                            <a href="carro.php">
+                                <div class="btn-verificar">Verificar</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <footer>
-        
     </footer>
 </body>
 </html>
